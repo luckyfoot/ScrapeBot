805040a759c426562dde24c23a18b1a3f35782e2
luckyfoot <solorio13@gmail.com>
2018-01-09 14:42:09 -0800

    Add files via upload

--- ScrapeBot.py
+++ ScrapeBot.py
@@ -6,7 +6,7 @@ import string
 
 def get_page():
     try:
-        clean_posts= []
+        """
         beg_address= "https://"
         mid_address= ".craigslist.org/search/rva?query="
         post_w_pic= "&hasPic=1"
@@ -20,7 +20,7 @@ def get_page():
         address= beg_adress + city + mid_address + first_search_param + post_w_pic + "&" + max_price + "&" + min_price
 
         print(address)
-        
+        """
         
         html = urlopen("https://seattle.craigslist.org/search/rva?query=trailer&hasPic=1&min_price=2000&max_price=10000")
         parsed_listings = BeautifulSoup(html, "html.parser")
@@ -30,45 +30,48 @@ def get_page():
 
     return clean_posts
 
+def wr_page(postings):
+    count = 0
+    
+    for post in postings:
+        for t in post:
+            title.append(t)
+        if 'href' in post.attrs:
+            address.append(post.attrs['href'])
+
+    for link in address:
+        count += 1
+        actualLink.append(link )
+    try:
+        scrapePage = open("trailerScrape.html", "w")
+        scrapePage.write("<html>")
+        scrapePage.write("<h1 style = \"font-weight:bold; font-size:200%\"> These are the top Craiglist postings for Trailers with a min. price of 2K</h1><br />")
+
+        for link in actualLink:
+            scrapePage.write("<a href= ")
+            scrapePage.write(link)
+            scrapePage.write(">")
+            scrapePage.write(title[count])
+            scrapePage.write("</a><br />")
+            count = count +1   
+        scrapePage.write("</html>")
+        scrapePage.close()
+
+    except:
+        print("Uh-oh something went wrong, unable to open the write file")
+        return
+
 craigslist = "https://seattle.craigslist.org"
-count = 0
 actualLink = []
 address = []
 title = []
 
-lengthTitle = len(address)
 
-postings = get_page()
+srch_rslts = get_page()
+wr_page(srch_rslts)
 
-for post in postings:
-    for t in post:
-        title.append(t)
-    if 'href' in post.attrs:
-        address.append(post.attrs['href'])
 
-for link in address:
-    count += 1
-    actualLink.append(link )
-try:
-    scrapePage = open("trailerScrape.html", "w")
-    scrapePage.write("<html>")
-    scrapePage.write("<h1 style = \"font-weight:bold; font-size:200%\"> These are the top Craiglist postings for Trailers with a min. price of 2K</h1><br />")
-
-    for link in actualLink:
-        scrapePage.write("<a href= ")
-        scrapePage.write(link)
-        scrapePage.write(">")
-        scrapePage.write(title[count])
-        scrapePage.write("</a><br />")
-        count = count +1   
-    scrapePage.write("</html>")
-    scrapePage.close()
-
-except:
-    print("Uh-oh something went wrong, unable to open the write file")
-    pass
-
-print('%d posts found that meet criteria' % count)
+print('I finished')
 
 
 		
